7214cbc8a093f2df6414157062efe40a12a4bff5
vicky-project <pratamavr@gmail.com>
2023-01-24 02:22:11 +0800

    edit tanggal

--- js/simplyCountdown.js
+++ js/simplyCountdown.js
@@ -115,9 +115,9 @@
      */
     simplyCountdown = function (elt, args) {
         var parameters = extend({
-                year: 2015,
-                month: 6,
-                day: 28,
+                year: 2023,
+                month: 3,
+                day: 15,
                 hours: 0,
                 minutes: 0,
                 seconds: 0,

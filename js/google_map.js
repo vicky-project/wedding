4d7141ed152333ff86ad11f4fa264a05c21ccd08
vicky-project <pratamavr@gmail.com>
2023-02-26 09:14:26 +0800

    add latlon

--- js/google_map.js
+++ js/google_map.js
@@ -4,9 +4,10 @@ function init() {
   // Basic options for a simple Google Map
   // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
   // var myLatlng = new google.maps.LatLng(40.71751, -73.990922);
-  var myLatlng = new google.maps.LatLng(40.69847032728747, -73.9514422416687);
+  var myLatlng = new google.maps.LatLng(-8.349476, 113.602652);
   // 39.399872
   // -8.224454
+  //-8.349476,113.602652
 
   var mapOptions = {
     // How zoomed in you want the map to start at (always required)
